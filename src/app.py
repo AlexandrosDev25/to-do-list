c1f0c241ea944628dfe7a90fc0a053a0011bf39c
Alexandros <rene_alejandro.m@outlook.com>
2025-08-10 22:19:14 -0500

    Modifica app.py como punto de entrada principal

--- src/app.py
+++ src/app.py
@@ -1,6 +1,13 @@
 from database import get_connection
+from todo_cli import main
 
 if __name__ == "__main__":
     print("🔌 Probando conexión a PostgreSQL...")
     conn = get_connection()
-    print("✅ ¡Conexión exitosa!" if conn else "❌ Falló la conexión")
+
+    if conn:
+        print("✅ ¡Conexión exitosa!")
+        conn.close()
+        main()  # Ejecuta la interfaz CLI
+    else:
+        print("❌ Falló la conexión")
